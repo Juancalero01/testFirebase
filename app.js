0d434ce30d3af85d2351de6329b36ae76c337d7f
Juan <marcelocalero5@gmail.com>
2021-05-09 20:33:51 -0300

    cambios

--- app.js
+++ app.js
@@ -1,49 +1,71 @@
 // Iniciar firebase
 var db = firebase.firestore();
 
+// Cargar pagina con datos
+window.onload = loadDate();
 
 // Guardar datos
 function saveDate() {
-    let nombre = document.getElementById('nombre').value;
+    let nombre = document.getElementById('nombre').value.toLowerCase();
     let tipo = document.getElementById('tipo').value;
     let edad = document.getElementById('edad').value;
     let fecha = document.getElementById('fecha').value;
-
-    db.collection("usuario").add({
-        nombre: nombre,
-        tipo: tipo,
-        edad: edad,
-        fecha: fecha
-    })
-        .then(function (docref) {
-            document.getElementById("form-principal").reset();
+    if (nombre, tipo, edad, fecha != '') {
+        db.collection("usuario").add({
+            nombre: nombre,
+            tipo: tipo,
+            edad: edad,
+            fecha: fecha
         })
-        .catch(function (error) {
-        })
-}
-
-// Leer datos
-let tabla = document.getElementById('table-body');
-db.collection("usuario").onSnapshot(function (querySnapshot) {
-    tabla.innerHTML = '';
-    querySnapshot.forEach(function (doc) {
-        tabla.innerHTML += `
-        <tr>
-        <td>${doc.data().nombre}</td>
-        <td>${doc.data().tipo} </td>
-        <td>${doc.data().edad} </td>
-        <td>${doc.data().fecha} </td>
-        <td><div class="btn-group" role="group">
-        <button type="button" class="btn btn-danger" onclick="eliminar('${doc.id}')"><i class="fas fa-user-minus"></i>
-        <button type="button" class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().nombre}','${doc.data().tipo}','${doc.data().edad}','${doc.data().fecha}')"><i class="fas fa-user-edit"></i>
-        </td>
+            .then(function (docref) {
+                document.getElementById("form-principal").reset();
+            })
+            .catch(function (error) {
+                let alert = document.getElementById("alert");
+                alert.innerHTML = '';
+                alert.innerHTML += `
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">Ups! Error al guardar.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                `
+            })
+    }
+    else {
+        let alert = document.getElementById("alert");
+        alert.innerHTML = '';
+        alert.innerHTML += `
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">Ups! Compruebe si todos los datos estan rellenados.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
         `
+    }
+
+}
+// Leer datos
+function loadDate() {
+    let tabla = document.getElementById('table-body');
+    db.collection("usuario").onSnapshot(function (querySnapshot) {
+        tabla.innerHTML = '';
+        querySnapshot.forEach(function (doc) {
+            tabla.innerHTML += `
+            <tr>            
+            <td>${doc.data().nombre[0].toUpperCase() + doc.data().nombre.slice(1)}</td>
+            <td>${doc.data().tipo} </td>
+            <td>${doc.data().edad} </td>
+            <td>${doc.data().fecha} </td>
+            <td><div class="btn-group" role="group">
+            <button type="button" class="btn btn-danger" onclick="deleteDate('${doc.id}')"><i class="fas fa-user-minus"></i>
+            <button type="button" class="btn btn-warning" onclick="editDate('${doc.id}','${doc.data().nombre}','${doc.data().tipo}','${doc.data().edad}','${doc.data().fecha}')"><i class="fas fa-user-edit"></i>
+            </td>
+            </tr>
+            `
+        })
     })
-})
-
-
+    // document.getElementById('btn-back') = '';
+    // document.getElementById('buscador').value = '';
+}
 // Borrar datos
-function eliminar(id) {
+function deleteDate(id) {
     let alert = document.getElementById("alert");
     db.collection("usuario").doc(id).delete().then(function () {
         alert.innerHTML = '';
@@ -62,9 +84,8 @@ function eliminar(id) {
         `
     })
 }
-
 // Editar datos
-function editar(id, nombre, tipo, edad, fecha) {
+function editDate(id, nombre, tipo, edad, fecha) {
     document.getElementById('nombre').value = nombre;
     document.getElementById('tipo').value = tipo;
     document.getElementById('edad').value = edad;
@@ -95,7 +116,7 @@ function editar(id, nombre, tipo, edad, fecha) {
                 button.innerHTML = 'AGREGAR'
                 document.getElementById("form-principal").reset();
             })
-            .catch(function () {
+            .catch(function (error) {
                 let alert = document.getElementById("alert");
                 alert.innerHTML = '';
                 alert.innerHTML += `
@@ -106,6 +127,52 @@ function editar(id, nombre, tipo, edad, fecha) {
             })
     }
 }
-
-
-
+// Buscar datos
+function searchName() {
+    let nombreBuscado = document.getElementById('buscador').value.toLowerCase();
+    if (nombreBuscado != '') {
+        let tabla = document.getElementById('table-body');
+        let volver = document.getElementById('btn-back');
+        volver.innerHTML = '';
+        volver.innerHTML += `
+        <button class="btn btn-link" onclick="loadDate()">Volver a la tabla </button>
+        `
+        db.collection("usuario").where("nombre", "==", nombreBuscado)
+            .get()
+            .then(function (querySnapshot) {
+                tabla.innerHTML = '';
+                querySnapshot.forEach(function (doc) {
+                    tabla.innerHTML += `
+                        <tr>
+                        <td>${doc.data().nombre[0].toUpperCase() + doc.data().nombre.slice(1)}</td>
+                        <td>${doc.data().tipo} </td>
+                        <td>${doc.data().edad} </td>
+                        <td>${doc.data().fecha} </td>
+                        <td><div class="btn-group" role="group">
+                        <button type="button" class="btn btn-danger" onclick="eliminar('${doc.id}')"><i class="fas fa-user-minus"></i>
+                        <button type="button" class="btn btn-warning" onclick="editar('${doc.id}','${doc.data().nombre}','${doc.data().tipo}','${doc.data().edad}','${doc.data().fecha}')"><i class="fas fa-user-edit"></i>
+                        </td>
+                        </tr>
+                        `
+                });
+            })
+            .catch(function (error) {
+                let alert = document.getElementById("alert");
+                alert.innerHTML = '';
+                alert.innerHTML += `
+                <div class="alert alert-danger alert-dismissible fade show" role="alert">Ups! Error al buscar nombre.
+                <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+                </div>
+                `
+            });
+    }
+    else {
+        let alert = document.getElementById("alert");
+        alert.innerHTML = '';
+        alert.innerHTML += `
+        <div class="alert alert-danger alert-dismissible fade show" role="alert">Ups! Compruebe si todos los datos estan rellenados.
+        <button type="button" class="btn-close" data-bs-dismiss="alert" aria-label="Close"></button>
+        </div>
+        `
+    }
+}
